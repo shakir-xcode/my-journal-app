a56b25945d4f818a58939523827685251a2bc4fe
Shakir <shaker88999@gmail.com>
2024-12-21 08:53:29 +0530

    first commit

---
+++ actions/public.js
@@ -0,0 +1,27 @@
+import { unstable_cache } from "next/cache";
+
+
+export const getDailyPrompt = unstable_cache(
+    async () => {
+        try {
+            const res = await fetch('https://api.adviceslip.com/advice', {
+                cache: "no-store"
+            });
+            const data = await res.json();
+            return {
+                success: true,
+                data: data?.slip?.advice
+            };
+        } catch (error) {
+            return {
+                success: false,
+                data: "What's on your mind today?"
+            };
+        }
+    },
+    ["daily-prompts"],
+    {
+        revalidate: 86400,
+        tags: ["daily-prompts"]
+    })
+
